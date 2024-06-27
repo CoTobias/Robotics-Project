cd4c94a8573817d971bec7437f150ea03dc79b42
Tobias <coptiltobias@gmail.com>
2024-06-27 14:23:51 +0200

    Testing Funcionality

--- BasicMovements.py
+++ BasicMovements.py
@@ -1,13 +1,19 @@
 from djitellopy import tello
 from time import sleep
+
 me = tello. Tello()
 me.connect()
 print(me.get_battery())
 
-me. takeoff()
+me.takeoff()
+# move forward -> - would be backwards
 me.send_rc_control(0, 50, 0, 0)
 sleep (2)
+# right movement -> - would be left
+me.send_rc_control(50, 0, 0, 0)
+sleep(2)
+
+#so that it does not need to land while moving (better not break it)
 me.send_rc_control(0, 0, 0, 0)
 sleep (2)
 me.land()
-gi
